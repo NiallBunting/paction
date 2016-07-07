726868420a12b8a3497212b0361cbadd0fcf2692
NiallBunting <niallbunting@googlemail.com>
2016-07-07 23:30:59 +0100

    Setting repo up for use
    This change added a requirements.txt. Updated the setup.py to something
    more sane as I learn how this stuff works.

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
-setup(
-    entry_points = {
+from setuptools import setup
+
+setup(name='threeiesms',
+      version='0.1',
+      description='Sends sms through Three(IE) webtext.',
+      url='http://github.com/niallbunting/threeiesms',
+      author='Niall Bunting',
+      author_email='',
+      license='MIT',
+      packages=['threeiesms'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False,
+      entry_points={
         'console_scripts': ['threeiesms=threeiesms.shell:main'],
-    }
+      }
 )
