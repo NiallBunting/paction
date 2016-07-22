222bdc0d4e43fd6f5520e27a6fea1927cd306fad
NiallBunting <niallbunting@googlemail.com>
2016-07-22 20:57:20 +0100

    Complete gather_main
    This function parses the main and validates parts of it.

--- paction/shell.py
+++ paction/shell.py
@@ -11,27 +11,50 @@ class Paction(object):
          except IndexError:
              raise IndexError('TODO print help')
 
-         config = configparser.RawConfigParser()
-         if config.read([action, os.path.expanduser('~/%s' % action)]) == []:
+         self.config = configparser.RawConfigParser()
+         if self.config.read([action, os.path.expanduser('~/%s' % action)]) == []:
              raise NameError('Config file not found')
 
-         if not config.has_section("main"):
+         if not self.config.has_section("main"):
              raise NameError("Config has no main section")
 
-         self.gather_main(config)
+         inputargs, actions, dicts = self.gather_main()
          # sort out inputs match them
          # get login pages
          # check input matches
          # do dictionarys
          # make sessions
 
-    def gather_main(self, config):
-        options = config.options("main")
-        # get actions
-          # validate they exist
-        # get cli
-          # mark as inputs
+    def get_type(self, options, keyword):
+        match = []
+        for key in options:
+            if keyword in key[0]:
+                match.append(key)
+        return match
 
+    def get_type_and_validate(self, section, keyword):
+        items = self.config.items(section)
+        matches = self.get_type(items, keyword)
+        for match in matches:
+            if not self.config.has_section(match[1]):
+                raise IndexError("%s section does not exist" % match[1])
+        return matches
+
+    def gather_main(self):
+
+        # Get actions and validate
+        actions = dict(self.get_type_and_validate("main", "action"))
+        # Get optdict and validate
+        optdict = dict(self.get_type_and_validate("main", "optionaldict"))
+
+        inputargs = self.config.get("main", "inputargs").split()
+
+        for arg in inputargs:
+            if "optionaldict" in arg:
+                if arg not in optdict:
+                    raise IndexError("%s is not a dictionary" % arg)
+
+        return inputargs, actions, optdict
 
 def check_string(data):
     if isinstance(data, six.string_types):
